3abfb136d0f718725421da7b1075d0771a89f605
TANKUT TAŞDELEN <Tankutt@users.noreply.github.com>
2021-01-23 18:56:31 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isim = new MyList<string>();
+            isim.Add("Tankut");
+            isim.Add("Kibar");
+            isim.Add("Kemal");
+            isim.Add("Gülsün");
+
+            foreach (var item in isim.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
